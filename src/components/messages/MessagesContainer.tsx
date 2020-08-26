3f8019243dcd504af86d8d2f3c50c6bc2b96b794
Karina1213 <karinaa121213@gmail.com>
2020-08-26 10:30:12 +0200

    added MessagesContainer.tsx

---
+++ src/components/messages/MessagesContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MessagesPage from './MessagesPage';
+import {Footer} from './Footer';
+
+
+export const MessagesContainer: React.FC = () => {
+
+    return (
+        <>
+            <MessagesPage/>
+            <Footer/>
+        </>
+    )
+}
